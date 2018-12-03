bd0d38be51354bf85b7c5657327e1f66b7e5e678
owenpaulmeyer <owenpaulmeyer@gmail.com>
2018-12-02 20:32:22 -0800

    not cleaned up both ways svg

--- src/main/scala/net/crinklejoint/SVGParser.scala
+++ src/main/scala/net/crinklejoint/SVGParser.scala
@@ -1,122 +1,200 @@
 package net.crinklejoint
 
-import java.awt.{Rectangle, Shape}
-import java.awt.geom.{AffineTransform, GeneralPath, PathIterator}
-import java.io.{File, FileOutputStream}
-import org.w3c.dom
-import org.w3c.dom.NodeList
-import javax.xml.parsers.DocumentBuilderFactory
-import javax.xml.parsers.DocumentBuilder
-import javax.xml.xpath.XPathFactory
-import javax.xml.xpath.XPathConstants
+import java.awt.{Color, Graphics2D}
+import java.awt.geom.{GeneralPath, PathIterator}
+import java.io.{File, FileOutputStream, IOException, OutputStreamWriter}
+import javax.xml.parsers.{DocumentBuilder, DocumentBuilderFactory}
+import javax.xml.xpath.{XPathConstants, XPathFactory}
+import org.apache.batik.anim.dom.SVGDOMImplementation
+import org.apache.batik.dom.GenericDOMImplementation
 import org.apache.batik.parser.{FloatArrayProducer, PointsParser}
+import org.apache.batik.svggen.SVGGraphics2D
+import org.w3c.dom
+import org.w3c.dom.{Node, NodeList}
+import org.apache.batik.parser.{AWTPathProducer, PathParser}
+import scala.collection.Seq
 
 
 object SVGParser {
-
-
-  def readFile(file: String) = {
+  def readFile(path: String): dom.Document = {
     val factory: DocumentBuilderFactory = DocumentBuilderFactory.newInstance
     val builder: DocumentBuilder = factory.newDocumentBuilder
-    val document: dom.Document = builder.parse(file)
+    val document: dom.Document = builder.parse(path)
+    document
+  }
+
+  def parseNode(document: dom.Document) = {
     val xpathExpression = "//path/@d"
     val xpf = XPathFactory.newInstance
     val xpath = xpf.newXPath
     val expression = xpath.compile(xpathExpression)
     val svgPaths = expression.evaluate(document, XPathConstants.NODESET).asInstanceOf[NodeList]
-
-
     val node = svgPaths.item(0).getNodeValue
-
-
-    println(s">>> $node")
-    parsePathShape(node)
-    "string"
     node
-
   }
 
-  import org.apache.batik.parser.AWTPathProducer
-  import org.apache.batik.parser.PathParser
-//  import java.awt.geom.Rectangle2D
-
-  def parsePathShape(svgPathShape: String) = try {
+  def parsePathShape(svgPathShape: String): PathIterator = try {
     val pathProducer = new AWTPathProducer
     val pathParser = new PathParser
     pathParser.setPathHandler(pathProducer)
     pathParser.parse(svgPathShape)
     val shape = pathProducer.getShape
-    val iterator: PathIterator = shape.getPathIterator(null)
-
-//    while (!iterator.isDone) {
-//
-//      """
-//        |>> (26.458332,196.9875)
-//        |>> (22.489582,193.41562)
-//        |>> (26.458332,189.84375)
-//        |>> (18.520832,189.84375)
-//        |>> (22.489582,193.41562)
-//        |>> (18.520832,196.9875)
-//      """.stripMargin
-//
-//      iterator.currentSegment(coords)
-//      println(s">> ${(coords(0), coords(1))}")
-//      iterator.next()
-//
-//      "next"
-//    }
-
-
-    iterator
+    val affineTransform = null
+    val pathIterator = shape.getPathIterator(affineTransform)
+    pathIterator
   }
 
-  def parsePoints() = {
-    val pointsProducer = new FloatArrayProducer
-    val pointsParser = new PointsParser
-    pointsParser.setPointsHandler(pointsProducer)
+  def parsePoints(points: Seq[(Float, Float)]) = {
+    val start = points.head
+    val path  = points.tail
+
+    val pathProducer = new AWTPathProducer
+    pathProducer.startPath()
+    (pathProducer.movetoAbs _).tupled(start)
+    for (point <- path) (pathProducer.linetoAbs _).tupled(point)
+    pathProducer.endPath
+    val shape = pathProducer.getShape
+    val affineTransform = null
+    shape.getPathIterator(affineTransform)
+  }
+
+  def createSVGPath(paths: Seq[PathIterator]) = {
+    val domImpl = GenericDOMImplementation.getDOMImplementation
+    val svgNS = null
+    val document = domImpl.createDocument(svgNS, "assvg", null)
+
+    val generalPath = new GeneralPath
+    for (path <- paths) generalPath.append(path, false)
+
+    val svgGenerator = new SVGGraphics2D(document)
+    svgGenerator.getShapeConverter.toSVG(generalPath).getAttribute("d")
+  }
+
+  def buildDocument(dAttribute: String)=  {
+    val impl = SVGDOMImplementation.getDOMImplementation
+    val svgNS = SVGDOMImplementation.SVG_NAMESPACE_URI
+    val doc = impl.createDocument(svgNS, "svg", null)
+    // get the root element (the svg element)
+    val svgRoot = doc.getDocumentElement
+    svgRoot.setAttribute("width", "210mm")
+    svgRoot.setAttribute("height", "297mm")
+    svgRoot.setAttribute("viewBox", "0 0 210 297")
+    svgRoot.setAttribute("version", "1.1")
+    svgRoot.setAttribute("id", "svg8")
+    svgRoot.setAttribute("viewBox", "0 0 210 297")
+    svgRoot.setAttribute("viewBox", "0 0 210 297")
+
+    val element = doc.createElementNS(svgNS, "path")
+    element.setAttribute("d", dAttribute)
+    element.setAttribute("style",
+      """fill:none;
+        |stroke:#000000;
+        |stroke-width:0.26458332px;
+        |stroke-linecap:butt;
+        |stroke-linejoin:miter;
+        |stroke-opacity:1""".stripMargin)
+    svgRoot.appendChild(element)
+    doc
+  }
+
+  def writeToFile(node: Node, path: String): Unit = {
+    import java.io.FileWriter
+    import javax.xml.transform.TransformerFactory
+    import javax.xml.transform.dom.DOMSource
+    import javax.xml.transform.stream.StreamResult
+
+    val source = new DOMSource(node)
+    val writer = new FileWriter(new File(path))
+    val result = new StreamResult(writer)
+
+    val transformerFactory = TransformerFactory.newInstance
+    val transformer = transformerFactory.newTransformer
+
+    import javax.xml.transform.OutputKeys
+    transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no")
+    transformer.setOutputProperty(OutputKeys.METHOD, "xml")
+    transformer.setOutputProperty(OutputKeys.INDENT, "yes")
+    transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8")
+    transformer.setOutputProperty(OutputKeys.MEDIA_TYPE, "UTF-8")
+    transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4")
+
+    transformer.transform(source, result)
   }
 
 
-  import org.apache.batik.dom.GenericDOMImplementation
-  import org.apache.batik.svggen.SVGGraphics2D
-  import org.w3c.dom.DOMImplementation
-  import java.awt.Color
-  import java.awt.Graphics2D
-  import java.io.IOException
-  import java.io.OutputStreamWriter
-  import java.io.Writer
 
-  def paint(g2d: Graphics2D): Unit = {
-//    g2d.setPaint(Color.red)
+
+
+
+
+
+
+
+
+
+
+//  def pointsToPath(points: Seq[Float]) = {
+//    val pathProducer = new AWTPathProducer
+//    val pathParser = new PathParser
+//    pathParser.setPathHandler(pathProducer)
+////    pathParser.parse(svgPathShape)
+//    val shape = pathProducer.getShape
+//    val pathIterator = shape.getPathIterator(null)
+//    pathIterator
+//  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  def paint(g2d: Graphics2D, paths: Seq[GeneralPath]): Unit = {
+    g2d.setPaint(Color.red)
     val path = new GeneralPath
     val iterator = parsePathShape("m 27.78125,107.69063 -3.96875,4.7625 -3.96875,-4.7625 0,8.33437 3.96875,-4.7625 3.96875,4.7625")
     path.append(iterator, false)
+//
+//    val p2 = new GeneralPath
+//    val i2 = parsePathShape("m 10.583333,204.13125 h 14.552083 v 9.525 l -6.614583,-4.7625 h -7.9375 z")
+//    p2.append(i2, false)
 
-    val p2 = new GeneralPath
-    val i2 = parsePathShape("m 10.583333,204.13125 h 14.552083 v 9.525 l -6.614583,-4.7625 h -7.9375 z")
-    p2.append(i2, false)
-    
-    g2d.draw(path)
-    g2d.draw(p2)
+    for (path <- paths) g2d.draw(path)
   }
 
   @throws[IOException]
-  def writeOut(): Unit = {
+  def createSvgGenerator(document: dom.Document, paths: Seq[GeneralPath]): Unit = {
     // Get a DOMImplementation.
-    val domImpl = GenericDOMImplementation.getDOMImplementation
+//    val domImpl = GenericDOMImplementation.getDOMImplementation
     // Create an instance of org.w3c.dom.Document.
-    val svgNS = "file:///home/owenpaulmeyer/src/nnw_gds/svg_test.svg"
-    val document = domImpl.createDocument(svgNS, "svg", null)
+//    val svgNS = "file:///home/owenpaulmeyer/src/nnw_gds/ver_simple.svg"
+//    val document = domImpl.createDocument(svgNS, "svg", null)
     // Create an instance of the SVG Generator.
-    val svgGenerator = new SVGGraphics2D(document)
+    val svgGenerator: SVGGraphics2D = new SVGGraphics2D(document)
     // Ask the test to render into the SVG Graphics2D implementation.
-    // val test = new TestSVGGen()
-    paint(svgGenerator)
+    paint(svgGenerator, paths)
+    svgGenerator
+
     // Finally, stream out SVG to the standard output using
     // UTF-8 encoding.
-    val useCSS = true
+//    val useCSS = false
     // we want to use CSS style attributes
-    val file = new File("dramp.svg")
+//    val file = new File("dramp.svg")
+//    val out = new OutputStreamWriter(new FileOutputStream(file), "UTF-8")
+//    svgGenerator.stream(out, useCSS)
+  }
+  def writeOut(svgGenerator: SVGGraphics2D, file: File): Unit = {
+    val useCSS = false
     val out = new OutputStreamWriter(new FileOutputStream(file), "UTF-8")
     svgGenerator.stream(out, useCSS)
   }
--- src/test/scala/samples/scalatest.scala
+++ src/test/scala/samples/scalatest.scala
@@ -1,128 +1,38 @@
-/*
- * Copyright 2001-2009 Artima, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package samples
 
-/*
-ScalaTest facilitates different styles of testing by providing traits you can mix
-together to get the behavior and syntax you prefer.  A few examples are
-included here.  For more information, visit:
 
-http://www.scalatest.org/
-
-One way to use ScalaTest is to help make JUnit or TestNG tests more
-clear and concise. Here's an example:
-*/
+import java.awt.geom.{GeneralPath, PathIterator}
+import java.io.File
 import net.crinklejoint.SVGParser
+import net.crinklejoint.SVGParser.parsePathShape
+import org.apache.batik.anim.dom.SVGDOMImplementation
+import org.apache.batik.dom.GenericDOMImplementation
+import org.apache.batik.parser.{AWTPathProducer, FloatArrayProducer, PointsParser}
+import org.apache.batik.svggen.SVGGraphics2D
+import org.scalatest.{FunSpec, Matchers}
+import org.w3c.dom.Node
 import scala.collection._
-import org.scalatest.Assertions
-import org.junit.Test
 
-class StackSuite extends Assertions {
+class SVGParserSpec extends FunSpec with Matchers {
 
-  @Test def stackShouldPopValuesIinLastInFirstOutOrder() {
-    val stack = new mutable.ArrayStack[Int]
-    stack.push(1)
-    stack.push(2)
-    assert(stack.pop() === 2)
-    assert(stack.pop() === 1)
-  }
+  describe("svg file io") {
 
-  @Test def stackShouldThrowRuntimeExceptionIfAnEmptyArrayStackIsPopped() {
-    val emptyStack = new mutable.ArrayStack[String]
-    intercept[RuntimeException] {
-      emptyStack.pop()
-    }
-  }
-}
-
-/*
-Here's an example of a FunSuite with Matchers mixed in:
-*/
-import org.scalatest.FunSuite
-import org.scalatest.Matchers
-
-import org.junit.runner.RunWith
-import org.scalatest.junit.JUnitRunner
-@RunWith(classOf[JUnitRunner])
-class ListSuite extends FunSuite with Matchers {
-
-  test("An empty list should be empty") {
-    List() should be ('empty)
-    Nil should be ('empty)
-  }
-
-  test("A non-empty list should not be empty") {
-    List(1, 2, 3) should not be ('empty)
-    List("fee", "fie", "foe", "fum") should not be ('empty)
-  }
-
-  test("A list's length should equal the number of elements it contains") {
-    List() should have length (0)
-    List(1, 2) should have length (2)
-    List("fee", "fie", "foe", "fum") should have length (4)
-  }
-}
-
-/*
-ScalaTest also supports the behavior-driven development style, in which you
-combine tests with text that specifies the behavior being tested. Here's
-an example whose text output when run looks like:
-
-A Map
-- should only contain keys and values that were added to it
-- should report its size as the number of key/value pairs it contains
-*/
-import org.scalatest.FunSpec
-
-class ExampleSpec extends FunSpec {
-
-  describe("An ArrayStack") {
-
-    it("should pop values in last-in-first-out order") {
-      val stack = new mutable.ArrayStack[Int]
-      stack.push(1)
-      stack.push(2)
-      assert(stack.pop() === 2)
-      assert(stack.pop() === 1)
-    }
-
-    it("should throw RuntimeException if an empty array stack is popped") {
-      val emptyStack = new mutable.ArrayStack[Int]
-      intercept[RuntimeException] {
-        emptyStack.pop()
-      }
-    }
-
-
-    it("should would") {
-      println(System.getProperty("user.dir"))
-      val node = SVGParser.readFile("ver_simple.svg")
+    it("should read an svg into a sequence of coordinates") {
+      val document = SVGParser.readFile("ver_simple.svg")
+      val node = SVGParser.parseNode(document)
       val iterator = SVGParser.parsePathShape(node)
 
       var coords: Array[Float] = new Array[Float](6)
       while (!iterator.isDone) {
 
-        """
-          |>> (26.458332,196.9875)
-          |>> (22.489582,193.41562)
-          |>> (26.458332,189.84375)
-          |>> (18.520832,189.84375)
-          |>> (22.489582,193.41562)
-          |>> (18.520832,196.9875)
-        """.stripMargin
+//        """
+//          |>> (26.458332,196.9875)
+//          |>> (22.489582,193.41562)
+//          |>> (26.458332,189.84375)
+//          |>> (18.520832,189.84375)
+//          |>> (22.489582,193.41562)
+//          |>> (18.520832,196.9875)
+//        """.stripMargin
 
 
 
@@ -133,8 +43,182 @@ class ExampleSpec extends FunSpec {
         "next"
       }
     }
-    it("will or willn't") {
-      SVGParser.writeOut
+
+    describe("parsePoints") {
+      it("should take a sequence of (float, float) and return a pathIterator") {
+        val points = Seq(
+          (26.458332f, 196.9875f),
+          (26.458332f, 196.9875f),
+          (22.489582f, 193.41562f),
+          (26.458332f, 189.84375f),
+          (18.520832f, 189.84375f),
+          (22.489582f, 193.41562f),
+          (18.520832f, 196.9875f)
+        )
+
+        val pathIterator = SVGParser.parsePoints(points)
+        val expectedPath = "M26.4583 196.9875 L26.4583 196.9875 L22.4896 193.4156 L26.4583 189.8438 L18.5208 189.8438 L22.4896 193.4156 L18.5208 196.9875"
+        SVGParser.createSVGPath(Seq(pathIterator)) shouldBe expectedPath
+      }
+    }
+
+    it("should export to disk") {
+
+      // Get a DOMImplementation.
+//    val domImpl = GenericDOMImplementation.getDOMImplementation
+      // Create an instance of org.w3c.dom.Document.
+//    val svgNS = "file:///home/owenpaulmeyer/src/nnw_gds/ver_simple.svg"
+//    val document = domImpl.createDocument(svgNS, "svg", null)
+      // Create an instance of the SVG Generator.
+
+
+      val domImpl = GenericDOMImplementation.getDOMImplementation
+      // Create an instance of org.w3c.dom.Document.
+      val svgNS = "file:///home/owenpaulmeyer/src/nnw_gds/dramp2.svg"
+      val document = domImpl.createDocument(svgNS, "svg", null)
+
+      val documentRead = SVGParser.readFile("ver_simple.svg")
+      // Create an instance of the SVG Generator.
+      val svgGenerator = new SVGGraphics2D(document)
+
+      val file = new File("dramp2.svg")
+
+      //      g2d.setPaint(Color.red)
+      val path = new GeneralPath
+      val iterator = SVGParser.parsePathShape("m 27.78125,107.69063 -3.96875,4.7625 -3.96875,-4.7625 0,8.33437 3.96875,-4.7625 3.96875,4.7625")
+      path.append(iterator, false)
+
+      SVGParser.createSvgGenerator(document, Seq(path))
+      SVGParser.writeOut(svgGenerator, file)
+    }
+    describe("createPath") {
+      it("should take a Seq of Paths and return an svg path string") {
+        val pI1 = parsePathShape(
+        "m 27.78125,107.69063 -3.96875,4.7625 -3.96875,-4.7625 0,8.33437 3.96875,-4.7625 3.96875,4.7625")
+        val pI2 = parsePathShape(
+        "m 10.583333,204.13125 h 14.552083 v 9.525 l -6.614583,-4.7625 h -7.9375 z")
+
+        val expectedPath = "M27.7812 107.6906 L23.8125 112.4531 L19.8438 107.6906 L19.8438 116.025 L23.8125 111.2625 L27.7812 116.025 M10.5833 204.1313 L25.1354 204.1313 L25.1354 213.6562 L18.5208 208.8938 L10.5833 208.8938 Z"
+        SVGParser.createSVGPath(Seq(pI1, pI2)) shouldBe expectedPath
+      }
+    }
+    it("split") {
+      val domImpl = GenericDOMImplementation.getDOMImplementation
+      val svgNS = null
+      val document = domImpl.createDocument(svgNS, "svg", null)
+      val pathShapeIterator = parsePathShape(
+        "m 27.78125,107.69063 -3.96875,4.7625 -3.96875,-4.7625 0,8.33437 3.96875,-4.7625 3.96875,4.7625")
+      val pathShapeIterator2 = parsePathShape(
+        "m 10.583333,204.13125 h 14.552083 v 9.525 l -6.614583,-4.7625 h -7.9375 z")
+
+//      val document = SVGParser.readFile("ver_simple.svg")
+      def createPath(paths: Seq[PathIterator]) = {
+        val generalPath = new GeneralPath
+        for (path <- paths) generalPath.append(path, false)
+
+        val svgGenerator = new SVGGraphics2D(document)
+        svgGenerator.getShapeConverter.toSVG(generalPath).getAttribute("d")
+      }
+
+
+//      def buildDocument(paths: Seq[String])=  {
+//        val impl = SVGDOMImplementation.getDOMImplementation
+//        val svgNS = SVGDOMImplementation.SVG_NAMESPACE_URI
+//        val doc = impl.createDocument(svgNS, "svg", null)
+//        // get the root element (the svg element)
+//        val svgRoot = doc.getDocumentElement
+//        svgRoot.setAttribute("width", "210mm")
+//        svgRoot.setAttribute("height", "297mm")
+//        svgRoot.setAttribute("viewBox", "0 0 210 297")
+//        svgRoot.setAttribute("version", "1.1")
+//        svgRoot.setAttribute("id", "svg8")
+//        svgRoot.setAttribute("viewBox", "0 0 210 297")
+//        svgRoot.setAttribute("viewBox", "0 0 210 297")
+//        // create the path element
+//        val element = doc.createElementNS(svgNS, "path")
+//        element.setAttribute("d",
+//          "m 26.458333,196.9875 -3.96875,-3.57188 3.96875,-3.57187 h -7.9375 l 2.645834,3.57188 -2.645834,3.57187")
+//        element.setAttribute("style",
+//          "fill:none;stroke:#000000;stroke-width:0.26458332px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1")
+//        svgRoot.appendChild(element)
+//        doc
+//      }
+      val doc = SVGParser.buildDocument("m 26.458333,196.9875 -3.96875,-3.57188 3.96875,-3.57187 h -7.9375 l 2.645834,3.57188 -2.645834,3.57187")
+
+
+
+
+//      val svgGenerator = createSvgGenerator(document, Seq(pathShapeIterator))
+      val node = createPath(Seq(pathShapeIterator, pathShapeIterator2))
+
+
+      val svgRoot = document.getDocumentElement
+
+// set the width and height attribute on the root svg element
+      svgRoot.setAttributeNS(null, "width", "210mm")
+      svgRoot.setAttributeNS(null, "height", "297mm")
+      svgRoot.setAttributeNS(null, "viewBox", "0 0 210 297")
+      // create the rectangle
+      val rectangle = document.createElementNS(null, "g")
+      rectangle.setAttributeNS(null, "x", "10")
+      rectangle.setAttributeNS(null, "y", "20")
+
+//      rectangle.setAttributeNS(null, "path",
+//        "m 10.583333,204.13125 h 14.552083 v 9.525 l -6.614583,-4.7625 h -7.9375 z")
+//      rectangle.setAttributeNS("g", "path", "wrtath")
+
+      // attach the rectangle to the svg root element
+      val generalPath = new GeneralPath
+      generalPath.append(pathShapeIterator, false)
+
+      val svgGenerator = new SVGGraphics2D(document)
+      svgGenerator.draw(generalPath)
+      svgRoot.appendChild(rectangle)
+
+
+
+
+
+
+
+//      def writeToFile(node: Node, path: String): Unit = {
+//        import java.io.FileWriter
+//        import javax.xml.transform.TransformerFactory
+//        import javax.xml.transform.dom.DOMSource
+//        import javax.xml.transform.stream.StreamResult
+//
+//        val source = new DOMSource(node)
+//        val writer = new FileWriter(new File(path))
+//        val result = new StreamResult(writer)
+//
+//        val transformerFactory = TransformerFactory.newInstance
+//        val transformer = transformerFactory.newTransformer
+//
+//        import javax.xml.transform.OutputKeys
+//        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no")
+//        transformer.setOutputProperty(OutputKeys.METHOD, "xml")
+//        transformer.setOutputProperty(OutputKeys.INDENT, "yes")
+//        transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8")
+//        transformer.setOutputProperty(OutputKeys.MEDIA_TYPE, "UTF-8")
+//        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4")
+//
+//        transformer.transform(source, result)
+//      }
+      SVGParser.writeToFile(document, "trance2.svg")
+      SVGParser.writeToFile(doc, "doc.svg")
+
+
+//      SVGParser.paint(svgGenerator)
+      // Finally, stream out SVG to the standard output using
+      // UTF-8 encoding.
+//      val useCSS = false
+      // we want to use CSS style attributes
+//      val file = new File("dramp.svg")
+//      val out = new OutputStreamWriter(new FileOutputStream(file), "UTF-8")
+//      svgGenerator.stream(out, useCSS)
+
+
+//      svgGenerator.stream("dramp_stamp.svg")
     }
   }
 }
